7252cd151a9d5ca278f7b330b11d6e5a65cb44d2
Peter Stiles <nobby@nobby.co.uk>
2019-11-11 13:00:34 +0000

    more sheet details!

--- src/main/java/com/nobbysoft/first/client/data/panels/CharacterSheet.java
+++ src/main/java/com/nobbysoft/first/client/data/panels/CharacterSheet.java
@@ -5,6 +5,11 @@ import java.awt.FlowLayout;
 import java.awt.GraphicsConfiguration;
 import java.awt.Window;
 import java.lang.invoke.MethodHandles;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.JEditorPane;
 import javax.swing.JLabel;
@@ -20,8 +25,18 @@ import com.nobbysoft.first.client.components.PDataComponent;
 import com.nobbysoft.first.client.components.PDialog;
 import com.nobbysoft.first.client.components.PPanel;
 import com.nobbysoft.first.client.components.PTextField;
+import com.nobbysoft.first.client.components.special.PComboGender;
 import com.nobbysoft.first.client.utils.MakeHTML;
+import com.nobbysoft.first.client.utils.Popper;
 import com.nobbysoft.first.common.entities.pc.PlayerCharacter;
+import com.nobbysoft.first.common.entities.pc.PlayerCharacterLevel;
+import com.nobbysoft.first.common.entities.staticdto.CharacterClass;
+import com.nobbysoft.first.common.entities.staticdto.Race;
+import com.nobbysoft.first.common.entities.staticdto.SavingThrow;
+import com.nobbysoft.first.common.servicei.CharacterClassService;
+import com.nobbysoft.first.common.servicei.RaceService;
+import com.nobbysoft.first.common.servicei.SavingThrowService;
+import com.nobbysoft.first.utils.DataMapper;
 
 @SuppressWarnings("serial")
 public class CharacterSheet extends PDialog {
@@ -33,6 +48,12 @@ public class CharacterSheet extends PDialog {
 
 	public void setPlayerCharacter(PlayerCharacter character) {
 		this.character=character;
+		try {
+			populateData();
+		} catch (SQLException e) {
+			Popper.popError(this, e);
+			return;
+		}
 		createSheet();
 	}
 	
@@ -44,7 +65,7 @@ public class CharacterSheet extends PDialog {
 	private JEditorPane edtHtml = new JEditorPane("text/html", "<h1>Hello</h1>");
 	private PTextField txtName = new PTextField();
 	private PTextField txtRace = new PTextField();
-	private PTextField txtGender = new PTextField();
+	private PComboGender txtGender = new PComboGender();
 	private PTextField txtClass = new PTextField();
 	
 	private PDataComponent[] disable = new PDataComponent[] {
@@ -67,36 +88,125 @@ public class CharacterSheet extends PDialog {
         btnSave.addActionListener(ae ->save());
         btnPrint.addActionListener(ae ->print());
 
-        pnlHead.add(new JLabel("Character"));
+        pnlHead.add(new JLabel("Character name"));
         pnlHead.add(txtName);
         pnlHead.add(new JLabel("Race"));
         pnlHead.add(txtRace);
         pnlHead.add(new JLabel("Gender"));
         pnlHead.add(txtGender);
-        pnlHead.add(new JLabel("Class"));
+        pnlHead.add(new JLabel("Class(es)"));
         pnlHead.add(txtClass);
         
         add(pnlHead,BorderLayout.NORTH);
         add(sclHtml,BorderLayout.CENTER);
         add(pnlButtons,BorderLayout.SOUTH);
-        edtHtml.setEditable(false);
-        //edtHtml.setContentType("text/html");
+        edtHtml.setEditable(false); 
         
         for(PDataComponent c:disable) {
         	c.setReadOnly(true);
         }
 	}
 
+	
+	private Map<String,List<SavingThrow>> savingThrows = new HashMap<>();
+	private Map<String,SavingThrow> bestSaves = new HashMap<>();
+	
+	private void populateData() throws SQLException{
+		CharacterClassService characterClassService = (CharacterClassService)DataMapper.INSTANCE.getDataService(CharacterClass.class);
+		RaceService raceService = (RaceService)DataMapper.INSTANCE.getDataService(Race.class);
+		SavingThrowService savingThrowService = (SavingThrowService)DataMapper.INSTANCE.getDataService(SavingThrow.class);
+		
+		race = raceService.get(character.getRaceId());
+		
+		CharacterClass c0 = characterClassService.get(character.getFirstClass());
+		characterClasses.put(c0.getClassId(),c0);
+		if(character.getSecondClass()!=null) {
+			CharacterClass c1 = characterClassService.get(character.getSecondClass());
+			characterClasses.put(c1.getClassId(),c1);			
+			if(character.getThirdClass()!=null) {
+				CharacterClass c2 = characterClassService.get(character.getThirdClass());
+				characterClasses.put(c2.getClassId(),c2);			
+			}
+
+		}
+		for(PlayerCharacterLevel pcd:character.getClassDetails()) {
+			if(pcd!=null) {
+				
+			String classId = pcd.getThisClass();
+			if(classId!=null) {
+				int lvl = pcd.getThisClassLevel();
+				savingThrows.put(classId,savingThrowService.getSavesForClassLevel(classId, lvl));			
+				}
+			} 
+		}
+		 {
+			// need to get just one set of saves
+			
+			for(String key:savingThrows.keySet()) {
+				List<SavingThrow> saves = savingThrows.get(key);
+				for(SavingThrow save:saves) {
+					String type=save.getSavingThrowTypeString();
+					if(!bestSaves.containsKey(type)) {
+						// add save in
+						bestSaves.put(type,save);
+					} else {
+						SavingThrow last = bestSaves.get(type);
+						
+							if(last.getRollRequired()>save.getRollRequired()) {
+								bestSaves.put(type,save);
+							}
+						}
+					}
+					
+				}
+			}
+			 
+	}
+	
+	private Map<String,CharacterClass> characterClasses = new HashMap<>();
+	private Race race = null;
+	
+	private String classy() {
+		
+
+		StringBuilder sb = new StringBuilder();
+		int i=0;
+		for(PlayerCharacterLevel pcd:character.getClassDetails()) {
+			if(pcd!=null) {
+				
+			String classId = pcd.getThisClass();
+			if(classId!=null) {
+			int xp =pcd.getThisClassExperience();
+			int hp =pcd.getThisClassHp();
+			int lvl = pcd.getThisClassLevel();
+			
+			CharacterClass cc = characterClasses.get(classId);
+			
+			if(i>0) {
+				sb.append(" / ");
+			}
+			sb.append(cc.getName()).append(" (").append(lvl).append(")");
+			
+			i++;
+			}
+			}
+		}		
+		
+		
+		return sb.toString();
+	}
 
 	private void createSheet(){
-		txtClass.setText(character.getFirstClass());
-		txtRace.setText(character.getRaceId());
-		txtGender.setText(character.getGender().name());
+		txtClass.setText(classy());
+		txtRace.setText(race.getName());
+		txtGender.setSelectedCode(character.getGender());
 		txtName.setText(character.getCharacterName());
 		//
 		//
+		List<SavingThrow> saves = new ArrayList<>();
+		saves.addAll(bestSaves.values());
 		MakeHTML make = new MakeHTML();
-		String html=make.makeDocument(character);
+		String html=make.makeDocument(character, characterClasses, race, saves);
 		edtHtml.setText(html);
         //edtHtml.setContentType("text/html");
 		LOGGER.info("html\n"+html);
